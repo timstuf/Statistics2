b593c8f1b442396c39729052dd77522ff52bb8d8
MiroJoseph01 <49228848+MiroJoseph01@users.noreply.github.com>
2020-04-15 16:52:57 +0300

    Idid

--- src/main/java/com/nure/Application.java
+++ src/main/java/com/nure/Application.java
@@ -2,9 +2,11 @@ package com.nure;
 
 public class Application {
     public static void main(String[] args) {
-       Solution solution = new Solution();
-        //solution.task1part1(25, 5.5, 6.6, 16, 0.1);
-        //solution.task1part2(25, 5.5, 6.6, 15.5, 0.02);
+
+        Solution solution = new Solution();
+        solution.task1part1(25, 5.5, 6.6, 16, 0.1);
+        solution.task1part2(25, 5.5, 6.6, 15.5, 0.02);
         solution.task2(25,15.5, 16,0.1);
+        solution.task3(new double[]{22, 26, 30, 34, 38, 42, 46, 50, 54, 58}, new int[]{0, 1, 3, 4, 4, 5, 4, 2, 1, 1}, 0.05);
     }
 }
