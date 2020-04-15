a9cf29e5bb5611612c0ab06ed9cdaea85cf706f1
Vlada <jagarioendl@gmail.com>
2020-04-15 18:22:46 +0300

    changed 1.1, 1.2 tasks

--- src/main/java/com/nure/Solution.java
+++ src/main/java/com/nure/Solution.java
@@ -14,17 +14,19 @@ public class Solution {
         System.out.println(" _____________________________________________");
         System.out.println();
         double kNabl = (sampleExpectation - generalExpectation) * Math.sqrt(n) / Math.sqrt(generalDispersion);
+        System.out.printf("H0: m = %.1f\n", generalExpectation);
+        System.out.printf("H1: m < %.1f\n\n", generalExpectation);
         System.out.printf("          (%.3f - %.3f) * sqrt(%d)\n", sampleExpectation, generalExpectation, n);
         System.out.printf("K nabl = ------------------------------    ==   %f\n", kNabl);
-        System.out.printf("               sqrt(%.3f)    \n", generalDispersion);
+        System.out.printf("               sqrt(%.3f)    \n\n", generalDispersion);
         double laplas = (1 - 2 * alpha) / 2;
-        System.out.printf("Ф(u кр) = (1 - 2 * alpha) / 2 = %.3f\n", laplas);
+        System.out.printf("Ф(u кр) = (1 - 2 * alpha) / 2 = (1 - 2 * %.2f) / 2 = %.3f\n", alpha, laplas);
         System.out.printf("Посмотрите на таблицу Лапласа. Найдите ячейку, значение которой равно %.2f. \n" +
                 "Сложите строку и колонку, которой соответствует это значение, и введите это число. \n(Например, если находится на пересечении 1.6 и 0.05 введите 1,65,\n РАЗДЕЛИТЕЛЬ - ЗАПЯТАЯ)\n", laplas);
         double uKr = scanner.nextDouble();
         System.out.printf("%.3f %s %.3f\n", kNabl, kNabl > -uKr ? ">" : "<", -uKr);
-        if (kNabl > -uKr) System.out.println("Нулевая гипотеза принимается");
-        else System.out.println("Нулевая гипотеза отвергается");
+        if (kNabl > -uKr) System.out.printf("Нулевая гипотеза H0: m = %.1f принимается\n", generalExpectation);
+        else System.out.printf("Нулевая гипотеза отвергается, так как не выполняется %.3f > %.3f\n", kNabl, -uKr);
     }
 
     public void task1part2(int n, double sampleExpectation, double generalExpectation, double dispersion, double alpha) {
@@ -33,15 +35,17 @@ public class Solution {
         System.out.println(" _____________________________________________");
         System.out.println();
         double kNabl = (sampleExpectation - generalExpectation) * Math.sqrt(n) / Math.sqrt(dispersion);
+        System.out.printf("H0: m = %.1f\n", generalExpectation);
+        System.out.printf("H1: m не равно %.1f\n\n", generalExpectation);
         System.out.printf("          (%.3f - %.3f) * sqrt(%d)\n", sampleExpectation, generalExpectation, n);
         System.out.printf("K nabl = ------------------------------    ==   %f\n", kNabl);
         System.out.printf("               sqrt(%.3f)    \n", dispersion);
         System.out.printf("Посмотрите на таблицу распределения Стьюдента. Число степеней свободы равно %d. \n" +
                 "Уровень значимости равен %.2f.\nВведите число на пересечении, РАЗДЕЛИТЕЛЬ - ЗАПЯТАЯ \n", n - 1, alpha);
         double t = scanner.nextDouble();
-        System.out.printf("%.3f %s %.3f\n", kNabl, kNabl > t ? ">" : "<", t);
-        if (kNabl < t) System.out.println("Нулевая гипотеза принимается");
-        else System.out.println("Нулевая гипотеза отвергается");
+        System.out.printf("|%.3f| %s %.3f\n", kNabl, Math.abs(kNabl) > t ? ">" : "<", t);
+        if (Math.abs(kNabl) < t) System.out.printf("Нулевая гипотеза H0: m = %.1f принимается\n", generalExpectation);
+        else System.out.printf("Нулевая гипотеза отвергается, так как не выполняется |%.3f| < %.3f", kNabl, t);
     }
 
     public void task3(double[] x, int[] n, double alpha) {
